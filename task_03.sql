5488b6a6f4cb9f72fb12f99aa34a91bec9c3b7b3
data-channel <chibalice@yandex.ru>
2026-07-23 12:52:38 +0300

    Delete task_03.sql

--- task_03.sql
+++
@@ -1,7 +0,0 @@
-select
-    co.country_name,
-    count(sh.shop_id) as shop_count
-from shops sh
-join cities c on sh.city_id = c.city_id 
-join countries co on c.country_id = co.country_id
-group by co.country_name order by shop_count desc;
