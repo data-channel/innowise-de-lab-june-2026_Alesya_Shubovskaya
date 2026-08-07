504eb4906281161f753673a893aac55b6fcb9dc8
data-channel <chibalice@yandex.ru>
2026-08-07 13:12:44 +0300

    Delete module_04_data_processing/lecture_01_data_quality/homework directory

--- module_04_data_processing/lecture_01_data_quality/homework/CHECK.sql
+++
@@ -1,9 +0,0 @@
-alter table silver.silver_employees
-add constraint check_hire_after_birth
-check (hire_date > birth_date);
-
-
-select constraint_name, constraint_type
-from information_schema.table_constraints
-where table_name = 'silver_employees' and table_schema = 'silver';
-
