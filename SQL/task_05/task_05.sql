63dd51258a23ea8142b695cda4aa70ecc6b410d4
data-channel <chibalice@yandex.ru>
2026-08-17 21:01:52 +0300

    Delete SQL directory

--- SQL/task_05/task_05.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-    e.first_name,
-    e.last_name,
-    sh.address,
-    s.total_price AS max_amount
-FROM sales s
-JOIN employees e ON s.employee_id = e.employee_id
-JOIN shops sh ON e.shop_id = sh.shop_id
-WHERE s.total_price = (SELECT MAX(total_price) FROM sales);
